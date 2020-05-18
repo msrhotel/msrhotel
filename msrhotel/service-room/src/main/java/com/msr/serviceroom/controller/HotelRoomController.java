5c0237bdd47701b75276f46aea92b47e4dd5b1bd
fzmsyx <sophiasaww@gmail.com>
2020-05-18 18:57:46 +0900

    客房管理模块(苏)

--- msrhotel/service-room/src/main/java/com/msr/serviceroom/controller/HotelRoomController.java
+++ msrhotel/service-room/src/main/java/com/msr/serviceroom/controller/HotelRoomController.java
@@ -7,12 +7,9 @@ import com.msr.serviceroom.service.HotelRoomService;
 import com.msr.serviceroom.service.HotelRoomtypeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,6 +38,74 @@ public class HotelRoomController {
     public R list(){
         List<HotelRoom> list = hotelRoomService.list(null);
         return R.ok().data("item", list);
+    }
+
+    @ApiOperation(value = "根据ID删除客房信息")
+    @DeleteMapping("{id}")
+    public R removeById(
+            @ApiParam(name = "id", value = "客房ID", required = true)
+            @PathVariable String id){
+        boolean result = hotelRoomService.removeById(id);
+        if(result){
+            return R.ok();
+        }else{
+            return R.error().message("删除失败");
+        }
+    }
+
+//    @ApiOperation(value = "分页讲师列表")
+//    @GetMapping("{page}/{limit}")
+//    public R pageQuery(
+//            @ApiParam(name = "page", value = "当前页码", required = true)
+//            @PathVariable Long page,
+//
+//            @ApiParam(name = "limit", value = "每页记录数", required = true)
+//            @PathVariable Long limit,
+//
+//            @ApiParam(name = "teacherQuery", value = "查询对象", required = false)
+//                    Query customerQuery){
+//
+//        Page<Customer> pageParam = new Page<>(page, limit);
+//
+//        customerService.pageQuery(pageParam, customerQuery);
+//        List<EduTeacher> records = pageParam.getRecords();
+//        long total = pageParam.getTotal();
+//
+//        return  R.ok().data("total", total).data("rows", records);
+//    }
+
+    @ApiOperation(value = "新增客房信息")
+    @PostMapping("save")
+    public R save(
+            @ApiParam(name = "room", value = "客房信息对象", required = true)
+            @RequestBody HotelRoom room){
+
+        hotelRoomService.save(room);
+        return R.ok();
+    }
+
+    @ApiOperation(value = "根据ID查询客房信息")
+    @GetMapping("{id}")
+    public R getById(
+            @ApiParam(name = "id", value = "客户ID", required = true)
+            @PathVariable String id){
+
+        HotelRoom room = hotelRoomService.getById(id);
+        return R.ok().data("item", room);
+    }
+
+    @ApiOperation(value = "根据ID修改客房信息")
+    @PutMapping("{id}")
+    public R updateById(
+            @ApiParam(name = "id", value = "客户ID", required = true)
+            @PathVariable String id,
+
+            @ApiParam(name = "room", value = "客户入住信息对象", required = true)
+            @RequestBody HotelRoom room){
+
+        room.setRoomNum(id);
+        hotelRoomService.updateById(room);
+        return R.ok();
     }
 
 }
